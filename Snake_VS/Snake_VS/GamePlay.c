0ca6967b873dce9423397bc0ce816a799a4a935b
60629194 <jim940502@gmail.com>
2023-11-15 21:21:18 +0800

    draft of GamePlay
    already can
    1. move with time&direction
    2. boundary limited
    3. collect coin

--- Snake_VS/Snake_VS/GamePlay.c
+++ Snake_VS/Snake_VS/GamePlay.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <conio.h>
+#include <time.h>
 
 //print background 
 void print_plate(int array[8][10]) {
@@ -48,22 +49,84 @@ void equalize_plate(int array1[8][10], int array2[8][10]) {
 void GamePlay() {
     int T[8][10];
     int M[8][10];
-    initialize_plate(M);
+
     initialize_plate(T);
 
-    M[3][1] = 1;
+    M[3][1] = 1;    //start point
     int coin = 0;
+    int a, b;
+    int snakePositionv = 3;
+    int snakePositionh = 1;
+    char direction='d';  //set initial=+x way
 
-    while (coin < 5) {
-        int a = rand() % 7;
-        int b = rand() % 9;
+    initialize_plate(M);
 
-        M[a][b] = 2;
 
-        print_plate(M);
+    while (1) {
+        if (snakePositionv > 8 || snakePositionh > 10 || snakePositionv < -1 || snakePositionh < -1) {//boundary check2
+            Sleep(3000);
+            break;
+        }
+        srand(time(0));
+        a = rand() % 7;
+        b = rand() % 9;
+
+        while (coin < 3) {
+            if (_kbhit()) {
+                char key = _getch();
+                switch (key) {  //keyboardhit reaction to change direction
+                case 'w':
+                    direction = 'w';
+                    break;
+                case 'a':
+                    direction = 'a';
+                    break;
+                case 's':
+                    direction = 's';
+                    break;
+                case 'd':
+                    direction = 'd';
+                    break;
+                }
+            }
+            initialize_plate(M);
+            M[a][b] = -1;   //coinPos
+            M[snakePositionv][snakePositionh] = 1;  //snakePos
+            print_plate(M);
+
+            switch (direction) {
+            case 'w':
+                snakePositionv--;
+                break;
+            case 'a':
+                snakePositionh--;
+                break;
+            case 's':
+                snakePositionv++;
+                break;
+            case 'd':
+                snakePositionh++;
+                break;
+            }
+            if (snakePositionv > 8 || snakePositionh > 10 || snakePositionv < -1 || snakePositionh < -1) {//boundary check1;
+                printf("game over");
+                Sleep(3000);
+                break;
+            }
+            Sleep(500);
+            system("cls");
+
+            if (a == snakePositionv && b == snakePositionh) {   //snakePos=coinPos
+                coin++;
+                break;
+            }
+        }
+        if (coin == 3) {
+            printf("Victory");
+            Sleep(3000);
+            break;
+        }
     }
 
-
-   
-    return 0;
+    return ;
 }
